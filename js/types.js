a86cb3ab3ccd89bc63770ecd806855f9746cb5e6
Lilia Lomaka <llomaka80@gmail.com>
2021-12-21 11:50:48 +0200

    Винятки приведення логічного типу

---
+++ js/types.js
@@ -0,0 +1,7 @@
+Boolean('')           // false
+Boolean(0)            // false     
+Boolean(-0)           // false
+Boolean(NaN)          // false
+Boolean(null)         // false
+Boolean(undefined)    // false
+Boolean(false)        // false
