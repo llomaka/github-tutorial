c518bc92b7c0d9dc9f406e0414432036fcc89bf9
Lilia Lomaka <llomaka80@gmail.com>
2021-12-26 16:27:32 +0200

    Static variables

---
+++ js/classCar.js
@@ -0,0 +1,30 @@
+class Car {
+  // Change code below this line
+  static MAX_PRICE = 50000;
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    if (newPrice > Car.MAX_PRICE) {
+        return;
+    }
+    this.#price = newPrice;
+  }
+  // Change code above this line
+}
+
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
